4fb784fe31ca8b4018bd0c484b3e5dd292f14f14
Irina <rennersha@mail.ru>
2023-10-12 17:48:21 +0300

    Добавлено решение финальной контрольной работы по основному блоку

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+["Hello", "2", "world", ":-)"] → ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] → ["-2"]
+["Russia", "Denmark", "Kazan"] → []
+*/
+
+Console.WriteLine("Введите элементы массива через пробел:");
+string input = Console.ReadLine();
+string[] inputArray = input.Split(new[] { ' ' },StringSplitOptions.RemoveEmptyEntries);
+
+int count = 0; 
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
+
+string[] resultArray = new string[count];
+int index = 0; 
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        resultArray[index] = inputArray[i];
+        index++;
+    }
+}
+
+string inputOutput = string.Join("\", \"", inputArray);
+string resultOutput = string.Join("\", \"", resultArray);
+Console.WriteLine($"[\"{inputOutput}\"] -> [\"{resultOutput}\"]");
